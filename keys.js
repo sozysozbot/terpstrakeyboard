39adce213db506fc8e42b33ba86cacea0146de8c
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2025-04-25 20:08:33 +0900

    高さマーカーの色を直す

--- keys.js
+++ keys.js
@@ -809,7 +809,7 @@ function drawHex(p, c) { /* Point, color */
     var scaleFactor = settings.hexSize / 50;
     settings.context.scale(scaleFactor, scaleFactor);
     settings.context.translate(10, -25);
-    settings.context.fillStyle = "white";
+    settings.context.fillStyle = getContrastYIQ(current_text_color);
     settings.context.font = "12pt linzklar_rounded";
     settings.context.textAlign = "center";
     settings.context.textBaseline = "middle";
